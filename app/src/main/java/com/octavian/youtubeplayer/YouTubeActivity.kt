0f76d3d068d6fc8699e07ac8546bb05523f79f77
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-13 00:51:22 +0300

    Improving the Player

--- app/src/main/java/com/octavian/youtubeplayer/YouTubeActivity.kt
+++ app/src/main/java/com/octavian/youtubeplayer/YouTubeActivity.kt
@@ -46,7 +46,10 @@ class YouTubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
         youTubePlayer?.setPlayerStateChangeListener(playerStateChangeListener)
         youTubePlayer?.setPlaybackEventListener(playbackEventListener)
         if (!wasRestored) {
-            youTubePlayer?.cueVideo(YOUTUBE_VIDEO_ID)
+//            youTubePlayer?.cueVideo(YOUTUBE_VIDEO_ID)
+            youTubePlayer?.loadVideo(YOUTUBE_VIDEO_ID)
+        } else {
+            youTubePlayer?.play()
         }
     }
 
